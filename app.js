5245dff730706aecd0b3315b8d84a1d60aa922e5
lisapps <lisa.ziegler@gmail.com>
2015-10-17 18:52:25 -0500

    created from angular.io tutorial

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+    var AppComponent = ng
+      .Component({
+        selector: 'my-app',
+        template: '<h1>My Second Angular 2 App</h1>'
+      })
+      .Class({
+        constructor: function () { }
+        });
+    document.addEventListener('DOMContentLoaded', function() {
+      ng.bootstrap(AppComponent);
+    });
+})();
